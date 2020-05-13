8307c8e280088e0e07dd54879d689b33ff041793
Kalle Ladefoged Pedersen <kalleladefogedpedersen@gmail.com>
2020-05-13 22:26:25 +0200

    Correct test project

---
+++ backend/gce.test/WeatherForecastTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using gce;
+using NUnit.Framework;
+
+namespace gce.test
+{
+  [TestFixture]
+  public class WeatherForecastTest
+  {
+    [Test]
+    public void ctor_test()
+    {
+      WeatherForecast wf = new WeatherForecast { Date = DateTime.Now.Date, Summary = "summary", TemperatureC = 23 };
+
+      Assert.AreEqual(DateTime.Now.Date, wf.Date);
+      Assert.AreEqual("summary", wf.Summary);
+      Assert.AreEqual(23, wf.TemperatureC);
+    }
+  }
+}
